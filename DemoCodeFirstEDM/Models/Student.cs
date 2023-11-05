13743a124712d7706dc678c54afb19dbc7f8b072
webnham5 <boychaypho93@gmail.com>
2023-11-05 21:51:14 +0700

    Entity Framework Core Final

---
+++ DemoCodeFirstEDM/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCodeFirstEDM.Models
+{
+    public class Student : Person
+    {
+        public Student()
+        {
+            Courses = new HashSet<Course>();
+        }
+        public string Group { get; set; }
+        public virtual Specialization Specialization { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+
+}
---
+++ DemoCodeFirstEDM/MyStockDBContext.cs
@@ -0,0 +1,33 @@
+﻿using DemoCodeFirstEDM.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCodeFirstEDM
+{
+    public class MyStockDBContext : DbContext
+    {
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<School> Schools { get; set; }
+        public virtual DbSet<Specialization> Specializations { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyStockDB"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
